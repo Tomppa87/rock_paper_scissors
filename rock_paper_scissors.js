844f8ce348dd4a34e6cb4904dcd62aa537e77143
Thomas Byrne <byrnesy987@gmail.com>
2025-01-06 10:30:09 +0200

    added scorecounter checkgame function for end

--- rock_paper_scissors.js
+++ rock_paper_scissors.js
@@ -8,6 +8,11 @@ const title = document.createElement("h1");
 title.classList.add("title");
 title.textContent = "Welcome to Rock, Paper or Scissors"
 
+const subtitle = document.createElement("h6");
+subtitle.classList.add("subtitle");
+subtitle.textContent = "Best of 5 rounds. Choose your weapon:";
+title.appendChild(subtitle)
+
 const buttons = document.querySelectorAll("button");
 console.log(buttons)
 
@@ -19,6 +24,7 @@ body.insertBefore(title, buttons[0])
 
 const container = document.createElement("div")
 container.classList.add("container");
+var textBox = container.textContent;
 container.textContent = ("You are playing round " + clickCounter + 
                         "\nThe score is currently: Human " + humanScore + " Computer "+computerScore)
 body.appendChild(container);
@@ -104,7 +110,8 @@ function playRound(humanChoice, computerChoice) {
         computerScore = computerScore +1;
         console.log("Computer wins");
     }
-
+    container.textContent = ("You chose " + humanChoice + ", the computer chose " + computerChoice+".\n You are playing round " + clickCounter + 
+        "\nThe score is currently: Human " + humanScore + " Computer "+computerScore)
     //console.log(humanScore)
     //console.log(computerScore)
 }
@@ -112,15 +119,47 @@ buttons.forEach((button) => {
     button.addEventListener("click", () => {
         //console.log(button.innerText.toLowerCase())
         var getHumanChoice = button.innerText.toLowerCase();
+        clickCounter += 1;
+        //console.log(clickCounter)
         console.log(getHumanChoice)
         
         var computerSelection = getComputerChoice()
         console.log(computerSelection)
         
         playRound(getHumanChoice, computerSelection)
+        checkGame()
         
     })
 })
+
+function checkGame() {
+    if (clickCounter < 6) {
+        console.log("next round")
+    } else {
+        resultbox = document.createElement("div")
+        resultbox.classList.add("resultbox");
+                
+        if (humanScore > computerScore) {
+            resultbox.textContent = "Game over \n You win!"
+            console.log("Game over")
+            console.log("You win!");
+            
+        }   else if (humanScore < computerScore) {
+            resultbox.textContent = "Game over \n You lose!"
+            console.log("Game over")
+            console.log("You lose");
+            
+        }   else {
+            resultbox.textContent = "Game over \n You both suck!"
+            console.log("Game over")
+            console.log("You both suck");
+            
+        }
+        container.textContent = ("Final Scores: Human " + humanScore + " Computer "+computerScore)
+        container.appendChild(resultbox)
+    }
+    
+}
 //var computerSelection = getComputerChoice();
 //console.log(computerSelection);
 //var humanSelection = getHumanChoice();
